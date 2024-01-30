cfc2c4b604ab441a996f7317a080905e47163a41
I.E.U. Juboraj Naofel <57951503+juborajnaofel@users.noreply.github.com>
2024-01-30 15:47:42 +0600

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+	$dbConfig = [
+		"DB_CONNECTION" => "mysql",
+		"DB_HOST"=>"127.0.0.1",
+		"DB_PORT"=>3306,
+		"DB_DATABASE"=>"testtenancy",
+		"DB_USERNAME"=>"root",
+		"DB_PASSWORD"=> ""
+	];
+?>
