76b9f5f90a130a7d6da6ae461dddb0913dac5259
I.E.U. Juboraj Naofel <57951503+juborajnaofel@users.noreply.github.com>
2024-01-30 15:50:55 +0600

    Update boot.php

--- src/boot/boot.php
+++ src/boot/boot.php
@@ -1,14 +1,14 @@
 <?php
 include(__DIR__.'/../connections/connection.php');
 include(__DIR__.'/../tenancy/tenancy.php');
+require_once(__DIR__.'../../../config/config.php');
 
-$db = new DatabaseConnection();
+$db = new DatabaseConnection($dbConfig);
 $db->connectCentral();
 $tenancy = new Tenancy($db);
 
 $subdomain = $tenancy->currentSubdomain();
 
-$tenancy->createTenant('testtenancy_'.$subdomain);
-$db->switchTenant('testtenancy_'.$subdomain);
-
+// $tenancy->createTenant('testtenancy_'.$subdomain);
+// $db->switchTenant('testtenancy_'.$subdomain);
 ?>
