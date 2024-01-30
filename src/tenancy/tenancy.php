e292f46f15c90bef39e3f15f58b4ed4f0337c7a1
I.E.U. Juboraj Naofel <57951503+juborajnaofel@users.noreply.github.com>
2024-01-30 14:52:38 +0600

    Create tenancy.php

---
+++ src/tenancy/tenancy.php
@@ -0,0 +1,30 @@
+<?php
+class Tenancy{
+  public $conn = null;
+
+  public function __construct($db) {
+    $this->conn = $db->conn;
+  }
+
+  public function createTenant($tenantDBName){
+    try {
+        $sql = "CREATE DATABASE IF NOT EXISTS $tenantDBName";
+        $this->conn->exec($sql);
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage()."<br>";
+    }
+  }
+
+  public function currentSubdomain(){
+        $host = $_SERVER['HTTP_HOST'];
+        $parts = explode('.', $host);
+        if (count($parts) >= 2) {
+            $subdomain = $parts[0];
+        } else {
+            $subdomain = null;
+        }
+        return $subdomain;
+  }
+}
+?>
+
