ea80a62a8eccf088fee75e078bdf0043b63554a5
I.E.U. Juboraj Naofel <57951503+juborajnaofel@users.noreply.github.com>
2024-02-02 20:46:51 +0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
 <?php
-	$dbConfig = [
-		"DB_CONNECTION" => "mysql",
-		"DB_HOST"=>"127.0.0.1",
-		"DB_PORT"=>3306,
-		"DB_DATABASE"=>"testtenancy",
-		"DB_USERNAME"=>"root",
-		"DB_PASSWORD"=> "",
-	];
+    $dbConfig = [
+        "DB_CONNECTION" => "mysql",
+        "DB_HOST" => "127.0.0.1",
+        "DB_PORT" => 3306,
+        "DB_DATABASE" => "testtenancy",
+        "DB_USERNAME" => "root",
+        "DB_PASSWORD" => "",
+    ];
 
-	$domainConfig = [
-		"APP_URL" => '',
-		"CENTRAL_DOMAIN => ''
-	];
+    $domainConfig = [
+        "APP_URL" => 'multidb-tenancy-main.test',
+        "CENTRAL_DOMAIN" => 'multidb-tenancy-main.test'
+    ];
 ?>
